e6badde2d3f94fe3b530dc5915119ceeecacab7a
Manjaka Randrianirina <manjaka.ran@onja.org>
2020-07-06 11:57:53 +0300

    Created a div and unordered list with three lists inside of its

--- dom_cardio/DOM-Cardio.js
+++ dom_cardio/DOM-Cardio.js
@@ -1,32 +1,81 @@
 // Make a div
 
+const div = document.createElement(`div`);
+
 // add a class of wrapper to it
+
+div.classList.add(`wrapper`);
 
 // put it into the body
 
+document.body.appendChild(div);
+
 // make an unordered list
+
+const unorderedList = document.createElement(`ul`);
 
 // add three list items with the words "one, two three" in them
 
+const firstList = document.createElement(`li`);
+const secondList = document.createElement(`li`);
+const thirdList = document.createElement(`li`);
+
+firstList.textContent = `One`;
+secondList.textContent = `Two`;
+thirdList.textContent = `Third`;
+
+
 // put that list into the above wrapper
+
+unorderedList.appendChild(firstList);
+unorderedList.appendChild(secondList);
+unorderedList.appendChild(thirdList);
+
+div.appendChild(unorderedList);
+
 
 // create an image
 
+const image = document.createElement(`img`);
+
 // set the source to an image
 
+image.setAttribute(`src`, ``);
+
+
 // set the width to 250
+
+image.setAttribute(`width`, `250`);
 
 // add a class of cute
 
 // add an alt of Cute Puppy
 
+image.setAttribute(`alt`, `Cute Pupy`);
+
 // Append that image to the wrapper
+
+div.appendChild(image);
 
 // with HTML string, make a div, with two paragraphs inside of it
 // put this div before the unordered list from above
 
+const myHTML = `
+<div>
+  <p></p>
+  <p></p>
+</div>`
+
+
+unorderedList.insertAdjacentHTML('beforebegin', '<div><p></p><p></p></div>');
+
+console.log(div);
+
+
 // add a class to the second paragraph called warning
 // remove the first paragraph
+
+
 
 // create a function called generatePlayerCard that takes in three arguments: name, age, and height
 
